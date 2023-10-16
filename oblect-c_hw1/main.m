bdffd7c982c2d101d9cf1a29686cfe008973d136
dsail <dsail@yandex.ru>
2023-10-16 22:07:01 +0300

    Initial Commit

---
+++ oblect-c_hw1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  oblect-c_hw1
+//
+//  Created by Eugene Zvyagin on 16.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
